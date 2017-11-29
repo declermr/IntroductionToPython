526cb91b9ed3fff64ed3d01d08b869fff2552bf9
declermr <declermr@rose-hulman.edu>
2017-11-29 15:18:04 -0500

    I completed m5

--- src/m5_your_turtles.py
+++ src/m5_your_turtles.py
@@ -2,15 +2,15 @@
 Your chance to explore Loops and Turtles!
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
+         their colleagues and Matthew De Clerck.
 """
 ########################################################################
-# TODO: 1.
+# Done: 1.
 # On Line 5 above, replace  PUT_YOUR_NAME_HERE  with your own name.
 ########################################################################
 
 ########################################################################
-# TODO: 2.
+# Done: 2.
 #
 #  You should have RUN the PREVIOUS module and READ its code.
 #  (Do so now if you have not already done so.)
@@ -28,3 +28,47 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 #
 #  Don't forget to COMMIT your work by using  VCS ~ Commit and Push.
 ########################################################################
+import rosegraphics as rg
+
+window = rg.TurtleWindow()
+
+john = rg.SimpleTurtle('turtle')
+john.pen = rg.Pen('cyan', 10)
+john.speed = 10
+
+for k in range(10):
+    john.pen_down()
+    john.forward(30)
+    john.left(10)
+    john.right(70)
+    john.pen_up()
+    john.backward(50)
+    john.pen_up()
+    john.left(20)
+    john.pen_up()
+
+john.pen = rg.Pen('blue', 5)
+
+for k in range(12):
+    john.pen_down()
+    john.forward(30)
+    john.left(10)
+    john.right(70)
+    john.pen_up()
+    john.backward(50)
+    john.pen_up()
+    john.left(30)
+    john.pen_up()
+
+john.pen = rg.Pen('green', 3)
+
+for k in range(20):
+    john.pen_down()
+    john.forward(30)
+    john.left(10)
+    john.right(70)
+    john.pen_up()
+    john.backward(50)
+    john.pen_up()
+    john.left(40)
+    john.pen_up()
